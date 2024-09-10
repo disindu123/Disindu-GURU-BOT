656d2a75e51b705c0c82edbac27da7a8b9f488bb
disindu123 <disindudamsandu@gmail.com>
2024-09-10 23:06:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ import fs from 'fs'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const ownervb = process.env.OWNERS || "94775704025;DISINDU;94775704025;DAMSANDU"
+const ownervb = process.env.OWNERS || "94775704025;💫Disindu Damsandu💫"
 
 
 const ownerlist = ownervb.split(';');
@@ -23,9 +23,9 @@ for (let i = 0; i < ownerlist.length; i += 2) {
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['917605902011', '32460248586']
-global.prems = ['917605902011', '32460248586', '919398758484']
-global.allowed = ['917605902011', '32460248586', '19152999993']
+global.mods = ['94775704025', '32460248586']
+global.prems = ['94775704025', '32460248586', '94775704025']
+global.allowed = ['94775704025', '32460248586', '94775704025']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = [
